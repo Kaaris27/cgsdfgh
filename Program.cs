70b293111e3ca4a04c70b78aae0435f00e4aff29
Kaaris27 <thulurav2000@gmail.com>
2016-10-27 02:14:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using LeagueSharp;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Game.Time == 1.00)
+            {
+                Game.PrintChat("test");
+            }
+        }
+    }
+}
